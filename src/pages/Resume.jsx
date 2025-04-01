a68d830082376a6f8a559d03de3ce116a4bd3866
jaguarvusal <jaguarvusal@gmail.com>
2025-04-01 11:33:28 -0700

    updated resume proficiencies

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,12 +5,25 @@ const Resume = () => {
     <section id="resume">
       <h2>Resume</h2>
       <a href="/path-to-resume.pdf" download>Download Resume</a>
-      <h3>Proficiencies</h3>
+      <h3>Front-end Proficiencies</h3>
       <ul>
+        <li>HTML</li>
+        <li>CSS</li>
         <li>JavaScript</li>
         <li>React</li>
-        <li>Node.js</li>
-        <li>HTML & CSS</li>
+        <li>Responsive Design</li>
+        <li>Bootstrap</li>
+      </ul>
+      <h3>Back-end Proficiencies</h3>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>Postgres</li>
+        <li>Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>REST</li>
+        <li>GraphQL</li>
       </ul>
     </section>
   );
