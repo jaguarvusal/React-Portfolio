24d86e5b161ddf36ddb78f73be0dd011aced0cbc
jaguarvusal <jaguarvusal@gmail.com>
2025-04-01 13:54:01 -0700

    changed styling of projects added image as background

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,14 +2,33 @@ import React from "react";
 
 const Project = ({ project }) => {
   return (
-    <div className="project">
-      <h3>{project.title}</h3>
-        <a href={project.deployedLink} target="_blank" rel="noopener noreferrer">
-          Deployed App
+    <div
+      className="project"
+      style={{
+        backgroundImage: `url(${project.image})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      
+        <h3 className="project-title">{project.title}</h3>
+        <a
+          href={project.deployedLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="project-link"
+        >
+          Live Demo
         </a>
-        <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+        <a
+          href={project.githubLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="project-link"
+        >
           GitHub Repository
         </a>
+     
     </div>
   );
 };
