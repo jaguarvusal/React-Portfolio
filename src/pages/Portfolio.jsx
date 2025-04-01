7de036eeaa94f2cd07652a2037be879b9f2bfade
jaguarvusal <jaguarvusal@gmail.com>
2025-04-01 14:00:17 -0700

    changed dir of images

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -7,37 +7,37 @@ const Portfolio = () => {
       title: "Bootcamp Pizza Ordering Website", 
       deployedLink: "https://p1t1-group3.github.io/ProjectOne/", 
       githubLink: "https://github.com/p1t1-group3/ProjectOne",
-      image: "/src/assets/pizza.jpg" 
+      image: "./src/assets/pizza.jpg" 
     },
     { 
       title: "Weather Dashboard", 
       deployedLink: "https://weather-dashboard-application-y5xq.onrender.com/", 
       githubLink: "https://github.com/jaguarvusal/Weather-Dashboard-Application",
-      image: "/src/assets/weather.jpg" 
+      image: "./src/assets/weather.jpg" 
     },
     { 
       title: "Content Management System", 
       deployedLink: "#", 
       githubLink: "https://github.com/jaguarvusal/cms.git",
-      image: "/src/assets/cms.jpg" 
+      image: "./src/assets/cms.jpg" 
     },
     { 
       title: "Vehicle Maker", 
       deployedLink: "#", 
       githubLink: "https://github.com/jaguarvusal/Vehicle-Maker",
-      image: "/src/assets/vehicle.jpg" 
+      image: "./src/assets/vehicle.jpg" 
     },
     { 
       title: "README Generator", 
       deployedLink: "#", 
       githubLink: "https://github.com/jaguarvusal/README-Generator",
-      image: "/src/assets/readme.jpg" 
+      image: "./src/assets/readme.jpg" 
     },
     { 
       title: "React Portfolio", 
       deployedLink: "https://vusalcodingportfolio.netlify.app/", 
       githubLink: "https://github.com/jaguarvusal/React-Portfolio",
-      image: "/src/assets/portfolio.jpg" 
+      image: "./src/assets/portfolio.jpg" 
     }
   ];
 
