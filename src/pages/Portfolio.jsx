8fdb6d1fed6b21f2ca85291e081175e0c4e95027
jaguarvusal <jaguarvusal@gmail.com>
2025-04-01 14:09:06 -0700

    imported project images insead of using dir

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,43 +1,51 @@
 import React from "react";
 import Project from "../components/Project.jsx";
 
+// Import images
+import pizzaImage from "../assets/pizza.jpg";
+import weatherImage from "../assets/weather.jpg";
+import cmsImage from "../assets/cms.jpg";
+import vehicleImage from "../assets/vehicle.jpg";
+import readmeImage from "../assets/readme.jpg";
+import portfolioImage from "../assets/portfolio.jpg";
+
 const Portfolio = () => {
   const projects = [
     { 
       title: "Bootcamp Pizza Ordering Website", 
       deployedLink: "https://p1t1-group3.github.io/ProjectOne/", 
       githubLink: "https://github.com/p1t1-group3/ProjectOne",
-      image: "src/assets/pizza.jpg" 
+      image: pizzaImage
     },
     { 
       title: "Weather Dashboard", 
       deployedLink: "https://weather-dashboard-application-y5xq.onrender.com/", 
       githubLink: "https://github.com/jaguarvusal/Weather-Dashboard-Application",
-      image: "src/assets/weather.jpg" 
+      image: weatherImage
     },
     { 
       title: "Content Management System", 
       deployedLink: "#", 
       githubLink: "https://github.com/jaguarvusal/cms.git",
-      image: "src/assets/cms.jpg" 
+      image: cmsImage
     },
     { 
       title: "Vehicle Maker", 
       deployedLink: "#", 
       githubLink: "https://github.com/jaguarvusal/Vehicle-Maker",
-      image: "src/assets/vehicle.jpg" 
+      image: vehicleImage
     },
     { 
       title: "README Generator", 
       deployedLink: "#", 
       githubLink: "https://github.com/jaguarvusal/README-Generator",
-      image: "src/assets/readme.jpg" 
+      image: readmeImage
     },
     { 
       title: "React Portfolio", 
       deployedLink: "https://vusalcodingportfolio.netlify.app/", 
       githubLink: "https://github.com/jaguarvusal/React-Portfolio",
-      image: "src/assets/portfolio.jpg" 
+      image: portfolioImage
     }
   ];
 
